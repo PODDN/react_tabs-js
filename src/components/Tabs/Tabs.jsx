8e7cf084861fffa124dfe1dd158ea0ee6946fe67
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-09-13 20:47:24 +0300

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,24 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
+  <ul>
+    {tabs.map(tab => (
+      <li
+        key={tab.id}
+        data-cy="Tab"
+        className={tab.id === activeTabId ? 'is-active' : ''}
+      >
+        <a
+          href={`#${tab.id}`}
+          data-cy="TabLink"
+          onClick={e => {
+            e.preventDefault(); // щоб не стрибав до #tab-id
+            if (tab.id !== activeTabId) {
+              onTabSelected(tab.id);
+            }
+          }}
+        >
+          {tab.title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
